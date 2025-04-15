587b3b79caba187b893dec718b745472c66c43a8
MikeyMouse0 <lordworldpop@gmail.com>
2025-04-15 23:45:30 +0300

    main.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+
+BOT_TOKEN = 'ВАШ_ТОКЕН_БОТА'
+WEBAPP_URL = 'https://ВАШ_ДОМЕН/webapp/index.html'  # Замените на ваш URL
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    web_app = types.WebAppInfo(WEBAPP_URL)
+    markup.add(types.InlineKeyboardButton("Открыть Web App", web_app=web_app))
+    bot.send_message(message.chat.id, "Нажмите кнопку для открытия Web App:", reply_markup=markup)
+
+@bot.message_handler(content_types=['web_app_data'])
+def web_app_data(message):
+    bot.send_message(message.chat.id, f"Вы отправили из Web App: {message.web_app_data.data}")
+
+if __name__ == '__main__':
+    bot.polling() 
